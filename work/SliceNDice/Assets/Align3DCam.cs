065c766b067e798dbcc7773028fc9a19b95c1527
HendrallSchultz22 <42852571+HendrallSchultz22@users.noreply.github.com>
2020-01-31 15:22:08 -0500

    dying inside

---
+++ work/SliceNDice/Assets/Align3DCam.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class Align3DCam : MonoBehaviour
+{
+    public Transform A1;
+    public Transform A2;
+
+    public CinemachineVirtualCamera virtualCamera;
+
+    public CinemachineTransposer transposer;
+
+    public bool hasVirtualCamera;
+
+    public Vector3 framingNormal;
+
+    public float distance;
+
+    public float transposerLinearSlope;
+
+    public float transposerLinearOffset;
+
+    public float minDistance;
+
+    public float minCamDistance;
+
+    public float secondaryDistance;
+
+    public float secondaryCamDistance;
+
+    void CalculateSlopes()
+    {
+        if(virtualCamera == null)
+        {
+            return;
+        }
+        transposer = virtualCamera.GetCinemachineComponent<CinemachineTransposer>();
+        if(transposer == null)
+        {
+            return;
+        }
+
+        if (!Application.isPlaying)
+        {
+            minDistance = Vector3.Distance(A1.position, A2.position);
+            distance = minDistance;
+            minCamDistance = transposer.m_FollowOffset.magnitude;
+        }
+
+        transposerLinearSlope = (secondaryCamDistance - minCamDistance) / (secondaryDistance - minDistance);
+        transposerLinearOffset = minCamDistance - (transposerLinearSlope * minDistance);
+    }
+
+
+
+    private void Awake()
+    {
+        hasVirtualCamera = virtualCamera != null;
+        if (hasVirtualCamera)
+        {
+            transposer = virtualCamera.GetCinemachineComponent<CinemachineTransposer>();
+            if(transposer == null)
+            {
+                hasVirtualCamera = false;
+            }
+            else
+            {
+                framingNormal = transposer.m_FollowOffset;
+                framingNormal.Normalize();
+            }
+        }
+    }
+
+    void Update()
+    {
+        Vector3 diff = A1.position - A2.position;
+        distance = diff.magnitude;
+        diff.y = 0f;
+        diff.Normalize();
+        if (hasVirtualCamera)
+        {
+            transposer.m_FollowOffset = framingNormal * (Mathf.Max(minDistance, distance) * transposerLinearSlope * transposerLinearOffset);
+
+        }
+        if(Mathf.Approximately(0f, diff.sqrMagnitude))
+        {
+            return;
+        }
+
+        Quaternion q = Quaternion.LookRotation(diff, Vector3.up) * Quaternion.Euler(0, 90, 0);
+
+        Quaternion qA = q * Quaternion.Euler(0, 180, 0);
+
+        float angle = Quaternion.Angle(q, transform.rotation);
+
+        float angleA = Quaternion.Angle(qA, transform.rotation);
+
+        if(angle < angleA)
+        {
+            transform.rotation = q;
+        }
+        else
+        {
+            transform.rotation = qA;
+        }
+    }
+
+  
+   
+   
+}
--- work/SliceNDice/Assets/InputStuff/TPSpawn.cs
+++ work/SliceNDice/Assets/InputStuff/TPSpawn.cs
@@ -2,16 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Cinemachine;
 
 public class TPSpawn : MonoBehaviour
 {
     public GameObject P1;
     public GameObject P2;
     public PlayerInputManager manager;
-
     public InputDevice p1Device;
     public InputDevice p2Device;
-
+    public CinemachineTargetGroup targetGroup;
+    public Align3DCam align;
     public const int NOSPLITSCREEN = -1;
     public const string NOCONTROLSCHEME = null; 
 
@@ -29,10 +30,32 @@ public class TPSpawn : MonoBehaviour
 
         manager.playerPrefab = P1.gameObject;
         manager.JoinPlayer(0, NOSPLITSCREEN, NOCONTROLSCHEME, p1Device);
-
-
         manager.playerPrefab = P2.gameObject;
         manager.JoinPlayer(1, NOSPLITSCREEN, NOCONTROLSCHEME, p2Device);
+        targetGroup = GameObject.Find("TargetGroup1").GetComponent<CinemachineTargetGroup>();
+        align = GameObject.Find("TargetGroup1").GetComponent<Align3DCam>();
+        CinemachineTargetGroup.Target target1;
+        target1.target = P1.transform;
+        target1.weight = 1;
+        target1.radius = 1.7f;
+        CinemachineTargetGroup.Target target2;
+        target2.target = P2.transform;
+        target2.weight = 1;
+        target2.radius = 1.7f;
+        for (int i = 0; i < targetGroup.m_Targets.Length; i++)
+        {
+            if (targetGroup.m_Targets[i].target == null)
+            {
+                targetGroup.m_Targets.SetValue(target1, 0);
+                targetGroup.m_Targets.SetValue(target2, 1);
+                align.A1 = P1.transform;
+                align.A2 = P2.transform;
+                return;
+            }
+        }
+          
+
+
 
 
 
@@ -46,7 +69,7 @@ public class TPSpawn : MonoBehaviour
 
     private void OnJoin()
     {
-      //  manager.playerPrefab = P2.gameObject;
+       //manager.playerPrefab = P2.gameObject;
        // manager.JoinPlayer(1);
 
     }
